d14331d023b2799254267b09a00d082c2618c918
Diego <drbv27@gmail.com>
2022-01-03 15:37:17 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+/* function App(){} */
+const MiComponente = ({ miProp }) => {
+  /* const { miProp } = props; */
+  return <div>Nombre: {miProp}</div>;
+};
+const App = () => {
+  return <MiComponente miProp={"Orion Enojado"} />;
+};
 
 export default App;
