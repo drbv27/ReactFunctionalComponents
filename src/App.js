7470a582593ffa549492c234d1e32829baa7bbaa
Diego <drbv27@gmail.com>
2022-01-04 07:33:52 -0500

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,44 +1,34 @@
-import { useState, useEffect, Component } from "react";
+import { useReducer, useState } from "react";
 
-const useContador = (inicial) => {
-  const [contador, setContador] = useState(inicial);
-  const incrementar = () => {
-    setContador(contador + 1);
-  };
-  return [contador, incrementar];
+//const state = { contador:0 }
+// action = {type:"un string",payload:lo que queramos(any)}
+const inicial = { contador: 0 };
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "incrementar":
+      return { contador: state.contador + 1 };
+    case "decrementar":
+      return { contador: state.contador - 1 };
+    case "set":
+      return { contador: action.payload };
+    default:
+      return state;
+  }
 };
-/* const Interval = ({ contador }) => {
-  useEffect(() => {
-    const i = setInterval(() => console.log(contador), 1000);
-    return () => clearInterval(i);
-  }, [contador]);
-  return <p>Intervalo</p>;
-}; */
-class Interval extends Component {
-  intervalo = "";
-  componentDidMount() {
-    this.intervalo = setInterval(() => console.log(this.props.contador), 1000);
-  }
 
-  componentWillUnmount() {
-    clearInterval(this.intervalo);
-  }
-  render() {
-    return <p>Intervalo</p>;
-  }
-}
 const App = () => {
-  const [contador, incrementar] = useContador(0);
-  useEffect(() => {
-    document.title = contador;
-    /* console.log("soy un efecto"); */
-  }, [contador]);
+  const [state, dispatch] = useReducer(reducer, inicial);
+  const [valor, setValor] = useState(0);
 
   return (
     <div>
-      Contador:{contador}
-      <button onClick={incrementar}>Incrementar</button>
-      <Interval contador={contador} />
+      Contador: {state.contador}
+      <input value={valor} onChange={(e) => setValor(e.target.value)} />
+      <button onClick={() => dispatch({ type: "incrementar" })}>+</button>
+      <button onClick={() => dispatch({ type: "decrementar" })}>-</button>
+      <button onClick={() => dispatch({ type: "set", payload: valor })}>
+        set
+      </button>
     </div>
   );
 };
